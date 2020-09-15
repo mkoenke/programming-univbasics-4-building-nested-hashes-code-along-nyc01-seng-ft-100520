ad0b70ab3d7d542e76e85a62fb665adc9253da0d
Mary Koenke <mkoenke@sas.upenn.edu>
2020-09-15 13:57:48 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -23,7 +23,7 @@ def fourth_challenge
       :heroine_friends => [{
         :name => "Steven",
         :age => "30",
-        :atttitude => "confused"
+        :attitude => "confused"
       },
       {
         :name => "Nurse",
